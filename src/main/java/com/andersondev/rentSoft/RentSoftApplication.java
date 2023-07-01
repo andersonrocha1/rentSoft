6eca4bf6e40248df1c7d0deb675c7ecf27b5352d
Anderson Rocha <anderson.tec.informacao@gmail.com>
2023-07-01 11:23:39 -0300

    Primeiro Commit

---
+++ src/main/java/com/andersondev/rentSoft/RentSoftApplication.java
@@ -0,0 +1,13 @@
+package com.andersondev.rentSoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentSoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentSoftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andersondev/rentSoft/RentSoftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andersondev.rentSoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentSoftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
